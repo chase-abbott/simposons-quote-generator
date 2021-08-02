2140865bf2bec45c24fcdf9aa159ae7792cc305d
Chase <cabbott93@gmail.com>
2021-08-02 16:50:42 -0700

    Change button text

--- src/components/app/character/Load.jsx
+++ src/components/app/character/Load.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function Load({ onLoad }){
-  return <button onClick={onLoad}> Get a Character!</button>;
+  return <button onClick={onLoad}> Get a Twin Peaks Quote!</button>;
 }
 
 Load.propTypes = {
